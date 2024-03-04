be71a9e454651416ad16766c2f0ef62663e9ca0c
Bishal Pahari <bishal.pahari53@gmail.com>
2024-03-04 22:01:57 +0545

    updated qn2 soln

--- qn2.js
+++ qn2.js
@@ -18,14 +18,12 @@ class Solution {
       // if the greatest number is 0 or negative, return 1
       return smallestPositiveInt;
     } else {
-      for (let i = 0; i < this.arr.length; i++) {
-        for (let j = 1; j <= greatest; j++) {
-          // loop through the numbers from 1 to the greatest number
-          if (!this.arr.includes(j)) {
-            // if the number is not in the array, return it
-            smallestPositiveInt = j;
-            return smallestPositiveInt;
-          }
+      for (let j = 1; j <= greatest; j++) {
+        // loop through the numbers from 1 to the greatest number
+        if (!this.arr.includes(j)) {
+          // if the number is not in the array, return it
+          smallestPositiveInt = j;
+          return smallestPositiveInt;
         }
       }
     }
@@ -34,7 +32,7 @@ class Solution {
   }
 }
 
-let arr = [1, 2, 3, 4];
+let arr = [1, 2, 4];
 let soln = new Solution(...arr);
 let returnValue = soln.smallestPositiveInteger();
 console.log(`Smallest positive integer: ${returnValue}`);
